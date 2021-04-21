c4351f47cbb8da4c61bb8600ba4b21460248e4f2
nosleep77 <49619557+nosleep77@users.noreply.github.com>
2021-04-21 14:51:26 -0400

    Update delete_broken_sym.py

--- delete_broken_sym.py
+++ delete_broken_sym.py
@@ -8,47 +8,39 @@ def main():
     in_file_names = load_file(open('in.txt').read())
     for in_file_name in in_file_names:
         print('-' * 40)
-        
-        # ./18321/ACQ767068IAAA.80.8 -> 18321/ACQ767068IAAA.80.8
-        dot_slash_removed = re.sub(r'^\./', '', in_file_name)
-        print(dot_slash_removed)
+        migr_path = in_file_name
+        migr_fname = splitall(migr_path)[-1] # ACQ767068IAAA.80.0
 
-        # spec /var/COOL/arscache1/odwmprod/migr/18321/ACQ767068IAAA.80.0
-        #  act /var/COOL/arscache1/odwmprod/migr/18321/ACQ767068IAAA.80.8
-        migr_path = os.path.join('/var/COOL/arscache1/odwmprod/migr/', dot_slash_removed)
-        print(migr_path)
-
-        # >>> re.search('(?<=/)([A-Z]{3})(\w+)', '18321/ACQ767068IAAA.80.8').groups()
+        # >>> re.search('([A-Z]{3})(\w+)', 'ACQ767068IAAA.80.0').groups()
         # ('ACQ', '767068IAAA')
-        three_letters, rest_alnum = re.search('(?<=/)([A-Z]{3})(\w+)', dot_slash_removed).groups()
+        three_letters, rest_alnum = re.search('([A-Z]{3})(\w+)', migr_fname).groups()
         print(f"{migr_path} -> {three_letters}, {rest_alnum}")
 
         # spec /var/COOL/arscache1/odwmprod/retr/ACQ/DOC/767068IAAA
         #  act /var/COOL/arscache1/odwmprod/retr/ACQ/DOC/767068IAAA
-        at_retr_path = os.path.join(
+        retr_path = os.path.join(
             '/var/COOL/arscache1/odwmprod/retr/',
             three_letters,
             'DOC',
             rest_alnum
         )
-        print(at_retr_path)
+        print(retr_path)
 
-        # /var/COOL/arscache1/odwmprod/retr/ACQ/DOC/744FAAA
         # /var/COOL/arscache1/odwmprod/retr/OQN/DOC/744FAAA
-        if not os.path.lexists(at_retr_path):
-            print(f"ERROR {at_retr_path} - doesn't exist")
+        if not os.path.lexists(retr_path):
+            print(f"ERROR {retr_path} - doesn't exist")
             continue
 
-        if not os.path.islink(at_retr_path):
-            print(f"ERROR {at_retr_path} - not symlink")
+        if not os.path.islink(retr_path):
+            print(f"ERROR {retr_path} - not symlink")
             continue
 
-        if not os.path.exists(at_retr_path):
-            print(f"ERROR {at_retr_path} - broken symlink")
+        if not os.path.exists(retr_path):
+            print(f"ERROR {retr_path} - broken symlink")
             continue
 
-        retr_resolved_path = os.path.realpath(at_retr_path)
-        print(f"{at_retr_path} -> {retr_resolved_path}")
+        retr_resolved_path = os.path.realpath(retr_path)
+        print(f"{retr_path} -> {retr_resolved_path}")
 
         if 'cache_migr' in retr_resolved_path:
             # ... then delete the related migr dir (NOT the retr)
@@ -72,6 +64,22 @@ def load_file(file_data):
             continue
         lines.append(stripped)
     return lines
+
+
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
 
 
 if __name__ == "__main__":
